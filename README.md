829268594f8f76ec78843240b65ed32a759c3273
mfks17 <mfks17@gmail.com>
2016-03-18 17:39:31 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+App Store Review Times Bitbar Plugin
+===
---
+++ average_app_store_review_times.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+# <bitbar.title>Reivew Average</bitbar.title>
+# <bitbar.version>v0.1.0</bitbar.version>
+# <bitbar.author>mfks17</bitbar.author>
+# <bitbar.author.github>mfks17</bitbar.author.github>
+# <bitbar.desc>Average App Store Review Times</bitbar.desc>
+# <bitbar.dependencies>ruby, nokogiri</bitbar.dependencies>
+
+require 'open-uri'
+require 'nokogiri'
+
+url = 'http://appreviewtimes.com/'
+
+charset = nil
+html = open(url) do |f|
+  charset = f.charset
+  f.read
+  end
+
+doc = Nokogiri::HTML.parse(html, nil, charset)
+
+doc.xpath('/html/body/div[2]/div/div[2]/div[1]/p[1]').each do |node|
+  puts  "iOS App Store " + node.text
+end
